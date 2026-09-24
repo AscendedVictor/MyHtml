11f25b5f9c24ed4b2d2596ddf15f65780a1b0715
AscendedVictor <jinxeverythink@gmail.com>
2026-09-24 20:11:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 let filmButton = document.getElementById("filmButton")  // button id aldik
+let SilmeButton = document.getElementById("Film Button")
 let filmList = document.getElementById("filmList")  // film list aldik
+let filmler = JSON.parse(localStorage.getItem("filmler")) || []
+
+
+filmler.forEach(function(film) {
+    filmList.innerHTML += `
+    <div class="film-card">
+        <img src="${film.poster}" alt="Film">
+        <h3>${film.title}</h3>
+        <p>${film.yildiz}</p>
+        <p>${film.year}</p>
+        <p>${film.rating}</p>
+    </div>
+    `
+})
+
+    
 
 
 
 
+    
+
+
+
+
+
+
+
+ 
+
+    
  
 filmButton.addEventListener("click", function () { // tiklayinca fonkisoyn eklicez
     let filmName = prompt("Film adini girin") //kullanicidan yazi aldik
@@ -28,6 +56,21 @@ fetch(`https://www.omdbapi.com/?t=${filmName}&apikey=5dfe2bbf`)
     .then(Response => Response.json())
     .then(data => {
         console.log(data.Title)
+        filmler.push({
+            title : data.Title,
+            poster : data.Poster,
+            year : data.Year,
+            rating : data.imdbRating,
+            yildiz:yildiz
+
+            
+        })
+        
+        
+       localStorage.setItem("filmler",JSON.stringify(filmler))
+    
+
+
 
 filmList.innerHTML += `  
     <div class="film-card">
